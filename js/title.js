822d4fbc4910f63031650a46858ccb584e5b7f74
hmku <harrisonmku@gmail.com>
2018-07-16 01:36:33 -0700

    Add typing animation to title

---
+++ js/title.js
@@ -0,0 +1,41 @@
+$(function(){
+	$(".title").typed({
+		strings: ["Hi, I'm Harrison."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 70,
+		// time before typing starts
+        startDelay: 150,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+        contentType: 'html',
+        loop: false,
+	});
+});
+
+$(function(){
+	$(".subtitle").typed({
+		strings: ["B.S. Computer Science @ UCSD"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 20,
+		// time before typing starts
+        startDelay: 2500,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+        contentType: 'html',
+        loop: false,
+	});
+});
